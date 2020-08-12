21cea645da398d723fad4e91ccc76a7ef665124b
Bruno de Oliveira <brunooliveira.me@outlook.com>
2020-08-12 17:41:17 -0300

    script

--- backend/script.sql
+++ backend/script.sql
@@ -1,4 +1,5 @@
-use sql10359214;
+create database lndb;
+use lndb;
 
 create table tb_lista_negra (
 	id_lista_negra		int primary key auto_increment,
@@ -7,7 +8,23 @@ create table tb_lista_negra (
     dt_inclusao			datetime
 );
 
-select * from tb_lista_negra;
+create table tb_lista_fofa (
+	id_lista_fofa			int primary key auto_increment,
+    nm_fofura				varchar(100) not null,
+    ds_porque				varchar(200) not null,
+    bt_colocaria_potinho	bool,
+    dt_niver				date not null
+);
+
+
 
 insert into tb_lista_negra (nm_pessoa, ds_motivo, dt_inclusao)
 					values ('Jennifer Aniston', 'Me abandonou quando fiquei famoso', '2020-01-01');
+                    
+alter table tb_lista_negra 
+        add ds_local varchar(100) default 'Outro';
+        
+        
+select * from tb_lista_negra;
+select * from tb_lista_fofa;
+                    
